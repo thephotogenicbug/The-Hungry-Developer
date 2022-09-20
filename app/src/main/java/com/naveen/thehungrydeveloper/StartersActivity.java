71a11cb3833b35cecc32cd8629652b4fc8688a5f
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-20 18:50:48 +0530

    Create a LinearLayout and add ListView to it and call the id in StartersActivity

--- app/src/main/java/com/naveen/thehungrydeveloper/StartersActivity.java
+++ app/src/main/java/com/naveen/thehungrydeveloper/StartersActivity.java
@@ -2,12 +2,16 @@ package com.naveen.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class StartersActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starters);
+
+        ListView startersList = findViewById(R.id.list_view_starters);
     }
 }
