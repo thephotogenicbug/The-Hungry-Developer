80a7d8761185d09b7405a1d800d3455820912164
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-20 18:23:58 +0530

    Create StartersActivity and SetOnClickListener and Create an Intent

---
+++ app/src/main/java/com/naveen/thehungrydeveloper/StartersActivity.java
@@ -0,0 +1,13 @@
+package com.naveen.thehungrydeveloper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starters);
+    }
+}
