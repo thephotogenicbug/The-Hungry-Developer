b3223f4ee6b1fc7f32fe7a7d54f9bd1c3917290d
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-20 17:47:25 +0530

    Assign Id and Add ScrollView

--- app/src/main/java/com/naveen/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/naveen/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,20 @@
 package com.naveen.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView startersCard;
+    CardView mainCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainCard = findViewById(R.id.card_view_mains);
     }
 }
